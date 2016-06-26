d8224953e6a87629fb89edf41090542b7264ab7f
Jose Luis Rodriguez <jlpuma24@gmail.com>
2016-06-26 15:45:53 -0500

    Creando servicio para subir imagen al servidor - A la espera de la url, y tipo de respuesta (Cliente trabaja en ello)

--- app/src/main/java/colector/co/com/collector/network/ApiService.java
+++ app/src/main/java/colector/co/com/collector/network/ApiService.java
@@ -4,11 +4,14 @@ import colector.co.com.collector.model.request.GetSurveysRequest;
 import colector.co.com.collector.model.request.LoginRequest;
 import colector.co.com.collector.model.response.GetSurveysResponse;
 import colector.co.com.collector.model.response.LoginResponse;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Created by Jose Rodriguez on 11/06/2016.
@@ -28,4 +31,13 @@ public interface ApiService {
     @POST("service/form/all/")
     Call<GetSurveysResponse> getSurveys(@Body GetSurveysRequest surveysRequest,
                                         @Header("token") String token);
+
+    //Waiting for url and type of response.
+    @Multipart
+    @POST("<waiting for url>")
+    Call<Object> doStoreImage(@Part MultipartBody.Part image,
+                              @Part("extension") String extension,
+                              @Part("question_id") long question_id,
+                              @Part("survey_id") long survey_id,
+                              @Part("name") String name);
 }
