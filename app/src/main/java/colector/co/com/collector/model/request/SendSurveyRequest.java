791cdf9c66796e10c52b57cd95c70c4d7a03132e
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-06-29 01:14:03 -0400

    :scissors: Add just string answer to the SendSurveyRequest

--- app/src/main/java/colector/co/com/collector/model/request/SendSurveyRequest.java
+++ app/src/main/java/colector/co/com/collector/model/request/SendSurveyRequest.java
@@ -22,63 +22,14 @@ public class SendSurveyRequest {
     private List<IdInputValue> responses;
 
 
-
     public SendSurveyRequest(Survey survey) {
         this.colector_id = String.valueOf(AppSession.getInstance().getUser().getColector_id());
         this.form_id = String.valueOf(survey.getForm_id());
         this.longitud = survey.getInstanceLongitude();
         this.latitud = survey.getInstanceLatitude();
-        this.horaini  = survey.getInstanceHoraIni();
+        this.horaini = survey.getInstanceHoraIni();
         this.horafin = survey.getInstanceHoraFin();
         this.setResponsesData(survey.getInstanceAnswers());
-    }
-
-    public String getColector_id() {
-        return colector_id;
-    }
-
-    public void setColector_id(String colector_id) {
-        this.colector_id = colector_id;
-    }
-
-    public String getForm_id() {
-        return form_id;
-    }
-
-    public void setForm_id(String form_id) {
-        this.form_id = form_id;
-    }
-
-    public void setForm_longitude(String longitude) {
-        this.longitud =longitude;
-    }
-
-    public String getForm_longitude() {
-        return longitud;
-    }
-
-    public void setForm_latitude(String latitude) {
-        this.latitud =latitude;
-    }
-
-    public String getForm_latitude() {
-        return latitud;
-    }
-
-    public void setForm_horaini(String horaini) {
-        this.horaini =horaini;
-    }
-
-    public String getForm_horaini() {
-        return horaini;
-    }
-
-    public void setForm_horafin(String horafin) {
-        this.horafin =horafin;
-    }
-
-    public String getForm_horafin() {
-        return horafin;
     }
 
     public List<IdInputValue> getResponses() {
@@ -89,10 +40,11 @@ public class SendSurveyRequest {
         this.responses = responses;
     }
 
-    public void setResponsesData(List<IdValue> responsesData) {
+    private void setResponsesData(List<IdValue> responsesData) {
         responses = new ArrayList<>();
-        for (IdValue item: responsesData) {
-            responses.add(new IdInputValue(String.valueOf(item.getId()),item.getValue()));
+        for (IdValue item : responsesData) {
+            if (item.getmType() != 6)
+                responses.add(new IdInputValue(String.valueOf(item.getId()), item.getValue()));
         }
     }
 }
